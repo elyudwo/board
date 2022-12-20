e41232c4537702d3a568b6290e25dd17d9c2911e
elyudwo <dudqk9696@naver.com>
2022-12-20 16:20:17 +0900

    feat : 아이디로 회원조회, 전체 회원 목록 조회 기능 추가

--- src/main/java/jpapractice/jpaboard/repository/MemberRepository.java
+++ src/main/java/jpapractice/jpaboard/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -17,6 +18,15 @@ public class MemberRepository {
     public Long save(Member member) {
         em.persist(member);
         return member.getId();
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class,id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m",Member.class)
+                .getResultList();
     }
 
 }
