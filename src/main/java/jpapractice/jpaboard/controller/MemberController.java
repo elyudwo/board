ddea6f244ca088ea34e12101eab727754fe1f63b
elyudwo <dudqk9696@naver.com>
2022-12-22 14:29:49 +0900

    feat : 회원가입, 회원목록조회 기능, view 구현

--- src/main/java/jpapractice/jpaboard/controller/MemberController.java
+++ src/main/java/jpapractice/jpaboard/controller/MemberController.java
@@ -1,4 +1,55 @@
 package jpapractice.jpaboard.controller;
 
-public class MemberContorller {
+import jpapractice.jpaboard.domain.Member;
+import jpapractice.jpaboard.repository.MemberRepository;
+import jpapractice.jpaboard.service.MemberService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm",new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Member member = new Member();
+        member.setMemberId(form.getMemberId());
+        member.setMemberPasswd(form.getMemberPassWd());
+        member.setCollege(form.getCollege());
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members",members);
+
+        return "members/memberList";
+    }
 }
