2dd7ce096bf924774c6d4ba138ae351293f29a63
elyudwo <dudqk9696@naver.com>
2023-01-16 22:50:54 +0900

    feat : 변경감지 이용해 회원 정보 수정

--- src/main/java/jpapractice/jpaboard/repository/MemberRepository.java
+++ src/main/java/jpapractice/jpaboard/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package jpapractice.jpaboard.repository;
 import jpapractice.jpaboard.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -36,5 +37,12 @@ public class MemberRepository {
                 .getResultList();
     }
 
-
+    @Transactional
+    public void updateMember(Long userId, String memberId, String memberPassWd, String name, String college) {
+        Member member1 = findOne(userId);
+        member1.setMemberId(memberId);
+        member1.setMemberPasswd(memberPassWd);
+        member1.setName(name);
+        member1.setCollege(college);
+    }
 }
